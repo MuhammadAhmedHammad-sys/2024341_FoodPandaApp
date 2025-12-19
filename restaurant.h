c0bd2f0e164724e1899a9b8b26d929ea92ef2493
Abidkazmi7 <abidkazmi777@gmail.com>
2025-12-19 17:13:14 +0500

    Made few changes to restaurant.h

--- restaurant.h
+++ restaurant.h
@@ -5,4 +5,92 @@
 #include <string>
 using namespace std;
 
+class Restaurant
+{
+private:
+    string filename;
+    string adminUsername;
+    string adminPassword;
+    bool verified=false;
+
+public:
+    Restaurant()
+    {
+
+    }
+
+    bool verifyCredentials(string username, string password)
+    {
+        if (username == adminUsername && password == adminPassword)
+        {
+            verified = true;
+            return true;
+        }
+        return false;
+    }
+
+    void restauratWindow()
+    {
+        cout << "========= Restaurant Panel =========" << endl;
+        if (!verified)
+        {
+            string username, password;
+            cout << "Enter Chef Username: ";
+            cin >> username;
+            cout << "Enter Chef Password: ";
+            cin >> password;
+
+            if (!verifyCredentials(username, password))
+            {
+                cout << "Invalid credentials. Access denied." << endl;
+                return;
+            }
+        }
+
+        while (true)
+        {
+            cout << "Welcome, Chef!" << endl
+                 << "Loading Data..." << endl
+                 << endl;
+
+            int choice;
+            cout << "Menu:" << endl;
+            cout << "\t1. View Menu" << endl;
+            cout << "\t2. Add Item" << endl;
+            cout << "\t3. Remove Item" << endl;
+            cout << "\t4. Save and Exit Restaurant Panel" << endl;
+            cout << "\t5. Change Restaurant Credentials" << endl;
+            cout << "Enter your choice: ";
+            cin >> choice;
+            switch (choice)
+            {
+            case 1:
+
+                break;
+            case 2:
+            {
+
+                break;
+            }
+            case 3:
+            {
+
+                break;
+            }
+            case 4:
+ 
+                cout << "Data saved. Exiting Restaurant Panel." << endl;
+                return;
+            case 5:
+            {
+
+                break;
+            }
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+            }
+        }
+    }
+};
+
 #endif
