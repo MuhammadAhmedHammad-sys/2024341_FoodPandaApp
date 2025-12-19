46da1fb1f3f8e8f7168e59243641d130b223c19a
MuhammadAhmedHammad-sys <hammad.muhammadahmed@gmail.com>
2025-12-20 02:30:47 +0500

    Customers
    Customers and Order Histories

---
+++ Locations.h
@@ -0,0 +1,146 @@
+#ifndef LOCATIONS_H
+#define LOCATIONS_H
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <list>
+#include <queue>
+#include <fstream>
+using namespace std;
+
+class LocationGraph
+{
+private:
+    map<int, string> locations;  // Map of location code to Location
+    map<int, list<int>> adjList; // Adjacency list for the graph
+    string locationFile;
+    string edgeFile;
+
+public:
+    LocationGraph(string locationFile = "locations/location_codes.txt", string edgeFile = "locations/location_edges.txt")
+    {
+        this->locationFile = locationFile;
+        this->edgeFile = edgeFile;
+
+        // Read locations from file
+        ifstream locFile(locationFile);
+        int code;
+        string name;
+        while (locFile >> code >> name)
+        {
+            locations[code] = name;
+        }
+        locFile.close();
+
+        // Read edges from file
+        ifstream edgeFileHandle(edgeFile);
+        int from, to;
+        while (edgeFileHandle >> from >> to)
+        {
+            adjList[from].push_back(to);
+        }
+    }
+
+    bool addLocation(int code, string name)
+    {
+        if (locations.find(code) != locations.end())
+        {
+            return false;
+        }
+        locations[code] = name;
+        return true;
+    }
+
+    void addEdge(int from, int to, bool directed = false)
+    {
+        adjList[from].push_back(to);
+        if (!directed)
+            adjList[to].push_back(from); // Assuming undirected graph
+    }
+
+    void saveToFile()
+    {
+        // Save locations to file
+        ofstream locFile(locationFile);
+        for (const auto &loc : locations)
+        {
+            locFile << loc.first << " " << loc.second << endl;
+        }
+        locFile.close();
+
+        // Save edges to file
+        ofstream edgeFileHandle(edgeFile);
+        for (const auto &pair : adjList)
+        {
+            int from = pair.first;
+            for (int to : pair.second)
+            {
+                edgeFileHandle << from << " " << to << endl;
+            }
+        }
+        edgeFileHandle.close();
+    }
+
+    void printGraph()
+    {
+        for (const auto &pair : adjList)
+        {
+            cout << "Location " << pair.first << " (" << locations[pair.first] << "): ";
+            for (int neighbor : pair.second)
+            {
+                cout << neighbor << " (" << locations[neighbor] << ") ";
+            }
+            cout << endl;
+        }
+    }
+
+    list<int> findPath(int start, int end)
+    {
+        list<int> path;
+        // Simple BFS for pathfinding
+        map<int, int> parent;
+        map<int, bool> visited;
+        queue<int> q;
+
+        visited[start] = true;
+        q.push(start);
+
+        while (!q.empty())
+        {
+            int current = q.front();
+            q.pop();
+
+            if (current == end)
+                break;
+
+            for (int neighbor : adjList[current])
+            {
+                if (!visited[neighbor])
+                {
+                    visited[neighbor] = true;
+                    parent[neighbor] = current;
+                    q.push(neighbor);
+                }
+            }
+        }
+
+        // Reconstruct path
+        int current = end;
+        while (current != start)
+        {
+            path.push_front(current);
+            current = parent[current];
+        }
+        path.push_front(start);
+
+        return path;
+    }
+
+    ~LocationGraph()
+    {
+        saveToFile();
+    }
+};
+
+#endif
---
+++ customer.h
@@ -0,0 +1,299 @@
+#ifndef CUSTOMERS_H
+#define CUSTOMERS_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+struct Customer
+{
+    string username;
+    string password;
+    int location_code;
+};
+
+struct CustomerNode
+{
+    Customer data;
+    CustomerNode *next;
+    CustomerNode(Customer d, CustomerNode *n) : data(d), next(n) {};
+};
+
+class CustomerHashTable
+{
+private:
+    CustomerNode **table;
+    int size = 0;
+    int items = 0;
+    bool changes = false;
+
+public:
+    CustomerHashTable()
+    {
+        const string fileName = "customer/customers.txt";
+
+        ifstream file(fileName);
+        string line;
+        while (getline(file, line) && getline(file, line) && getline(file, line))
+            this->size++;
+        file.close();
+
+        this->size = 2 * size + 16;
+        this->table = new CustomerNode *[size];
+        for (int i = 0; i < size; i++)
+            table[i] = nullptr;
+
+        loadCustomers();
+    }
+
+    int hash(const string &user_name)
+    {
+        unsigned int hash = 5381;
+        for (char c : user_name)
+        {
+            hash = hash * 31 + c;
+        }
+        return hash % this->size;
+    }
+
+    void insert(const Customer &user)
+    {
+        int hash_code = hash(user.username);
+        CustomerNode *head = table[hash_code];
+        table[hash_code] = new CustomerNode{user, head};
+        items++;
+    }
+
+    void loadCustomers(const string &fileName = "customer/customers.txt")
+    {
+        ifstream file(fileName);
+        Customer temp;
+        string loc_code;
+        while (getline(file, temp.username) && getline(file, temp.password) && getline(file, loc_code))
+        {
+            temp.location_code = stoi(loc_code);
+            insert(temp);
+        }
+    }
+
+    void insertNew(const Customer &user)
+    {
+        insert(user);
+        this->changes = true;
+    }
+
+    Customer *find(const string &username)
+    {
+        int hash_code = hash(username);
+        CustomerNode *node = table[hash_code];
+        while (node)
+        {
+            if (node->data.username == username)
+                return &(node->data);
+            node = node->next;
+        }
+        return nullptr;
+    }
+
+    bool remove(const string &username)
+    {
+        int hash_code = hash(username);
+        CustomerNode *node = table[hash_code];
+        CustomerNode *prev = nullptr;
+
+        while (node)
+        {
+            if (node->data.username == username)
+            {
+                if (prev)
+                    prev->next = node->next;
+                else
+                    table[hash_code] = node->next;
+                delete node;
+                items--;
+                changes = true;
+                return true;
+            }
+            prev = node;
+            node = node->next;
+        }
+        return false;
+    }
+
+    bool saveToFile(const string &fileName = "customer/customers.txt")
+    {
+        ofstream file(fileName, ios::trunc);
+        if (!file.is_open())
+            return false;
+
+        for (int i = 0; i < size; i++)
+        {
+            CustomerNode *node = table[i];
+            while (node)
+            {
+                file << node->data.username << "\n";
+                file << node->data.password << "\n";
+                file << node->data.location_code << "\n";
+                node = node->next;
+            }
+        }
+
+        file.close();
+        changes = false;
+        return true;
+    }
+
+    ~CustomerHashTable()
+    {
+        if (changes)
+            saveToFile();
+
+        for (int i = 0; i < size; i++)
+        {
+            CustomerNode *node = table[i];
+            while (node)
+            {
+                CustomerNode *tmp = node;
+                node = node->next;
+                delete tmp;
+            }
+        }
+        delete[] table;
+    }
+
+    friend class CurrentCustomer;
+};
+
+struct OrderRecord
+{
+    int restrauntID;
+    int price;
+};
+
+struct OrderNode
+{
+    OrderRecord order;
+    OrderNode *next;
+    OrderNode(OrderRecord o, OrderNode *n) : order(o), next(n) {};
+};
+
+class CurrentCustomer
+{
+private:
+    CustomerHashTable *HashTable;
+    Customer user;
+    OrderNode *head; // store order history
+    bool verified;
+    bool changes;
+
+public:
+    CurrentCustomer(CustomerHashTable *table) : HashTable(table), head(nullptr), verified(false) {};
+
+    bool verify(const string &user_name, const string &password)
+    {
+        if (!HashTable)
+            return false;
+
+        Customer *found = HashTable->find(user_name);
+        if (!found)
+            return false;
+
+        if (found->password == password)
+        {
+            user = *found; // copy customer data
+            verified = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    bool login(const string &user_name, const string &password)
+    {
+        if (verify(user_name, password))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool signup(const string &user_name, const string &password, int &loc_code)
+    {
+        if (!HashTable)
+            return false;
+
+        Customer *found = HashTable->find(user_name);
+        if (found)
+            return false;
+
+        HashTable->insertNew({user_name, password, loc_code});
+        return login(user_name, password);
+    };
+
+    string fileName(const string &username)
+    {
+        return "customer/" + username + "_details.txt";
+    }
+
+    void clearOrders()
+    {
+        while (head)
+        {
+            OrderNode *tmp = head;
+            head = head->next;
+            delete tmp;
+        }
+    }
+
+    bool loadOrderHistory(const string &username)
+    {
+        clearOrders();
+
+        ifstream file(fileName(username));
+
+        if (file.is_open())
+        {
+            OrderRecord temp;
+            while (file >> temp.restrauntID && file >> temp.price)
+            {
+                head = new OrderNode(temp, head);
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool saveToFile()
+    {
+        ofstream file(fileName(user.username), ios::trunc);
+        if (!file.is_open())
+            return false;
+
+        for (OrderNode *current = head; current; current = current->next)
+        {
+            file << current->order.restrauntID << '\n';
+            file << current->order.price << '\n';
+        }
+
+        file.close();
+        changes = false;
+        return true;
+    }
+
+    ~CurrentCustomer()
+    {
+        if (changes)
+            saveToFile();
+
+        clearOrders();
+    }
+};
+
+#endif
